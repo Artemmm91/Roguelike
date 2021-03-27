6a67edac39a799ec25d6288d555803552f9f4148
Artem Mustafin <24766926+Artemmm91@users.noreply.github.com>
2021-03-27 14:30:20 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ mkdir backend
 cd backend
 pip3 install virtualenv
 python3 -m venv ./venv
-source venv_project/bin/activate
+source venv/bin/activate
 pip3 install -r ../requirements.txt
 cd ..
 python3 main.py
